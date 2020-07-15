21135b20b13a3dabdb7413b2d619ec66da983ef9
wucaixin <wucaixin@wufeng-network.com>
2020-07-15 15:54:20 +0800

    feat: 修改loading 项目工程化

--- react/github-hot-webpack/src/clg.js
+++
@@ -1,3 +0,0 @@
-export default function clg(msg) {
-  console.log(msg);
-}
--- react/github-hot-webpack/webpack.config.js
+++ react/github-hot-webpack/webpack.config.js
@@ -1,20 +1,60 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const {
+  CleanWebpackPlugin
+} = require("clean-webpack-plugin");
+
 module.exports = {
   // 环境
-  mode: 'development',
+  mode: "development",
+  //开启sourcemap
+  devtool: "cheap-module-eval-source-map",
   // 目标文件
-  entry: [path.resolve(__dirname, './src/index.js')],
+  entry: {
+    index: path.resolve(__dirname, "./src/index.js"),
+    other: path.resolve(__dirname, "./src/other.js"),
+  },
   // 自定义输出文件
   output: {
-      path: path.resolve(__dirname, './dist'), //路径
-      filename: 'bundle.js' //文件名称
+    path: path.resolve(__dirname, "./dist"), //路径
+    filename: "[name].bunld.js", //文件名称
+  },
+  // 配置webpack-dev-server
+  devServer: {
+    /**
+     * 日志模式  friendly-errors-webpack-plugin 插件可以优化输出
+     * errors-only  只在发生错误时触发
+     * minimal 只在发生错误或者有新的编译时输出
+     * none 没有输出
+     * normal 标准输出
+     * verbose 全部输出
+     */
+    contentBase: path.join(__dirname, "dist"),
+    stats: "errors-only",
+    //默认地址 localhost
+    host: "localhost",
+    //默认端口 8080
+    port: 8080,
+    //是否直接打开浏览器
+    open: true,
+    // 开启HMR
+    hot: true,
   },
   // 插件
   plugins: [
-
+    // new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: "battle.html",
+      title: "battle",
+      template:'src/battle.html',
+      chunks: ["index"],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "hot.html",
+      title: "hot",
+      template:'src/hot.html',
+      chunks: ["index"],
+    }),
   ],
-  // 给导入的文件制定规则
-  module: {
-
-  }
-}
+};
