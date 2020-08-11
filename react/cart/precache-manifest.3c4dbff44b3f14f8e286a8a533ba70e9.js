e61cffbcc32497a63ab08388da2f9ab784e35228
wucaixin <wucaixin@wufeng-network.com>
2020-08-11 16:05:25 +0800

    Updates

---
+++ react/cart/precache-manifest.3c4dbff44b3f14f8e286a8a533ba70e9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7cbf76eed26fa6a04976080804b57bf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0ad2369050ec2439332c",
+    "url": "./static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "514d00b4c3065109d0f7",
+    "url": "./static/css/4.e9c4dc44.chunk.css"
+  },
+  {
+    "revision": "78a503655ac875600141",
+    "url": "./static/css/main.5c4c4c00.chunk.css"
+  },
+  {
+    "revision": "0ad2369050ec2439332c",
+    "url": "./static/js/2.e5920f86.chunk.js"
+  },
+  {
+    "revision": "fbdb25c6a30511ca0525a561a8e4c009",
+    "url": "./static/js/2.e5920f86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce2ce60f0770ecd162f4",
+    "url": "./static/js/3.e75056d5.chunk.js"
+  },
+  {
+    "revision": "a7be164640a98ad548d77ef52146abc6",
+    "url": "./static/js/3.e75056d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "514d00b4c3065109d0f7",
+    "url": "./static/js/4.f481d6dd.chunk.js"
+  },
+  {
+    "revision": "e744e390595e4ec526aa",
+    "url": "./static/js/5.bc1c9f66.chunk.js"
+  },
+  {
+    "revision": "78a503655ac875600141",
+    "url": "./static/js/main.9cabdf86.chunk.js"
+  },
+  {
+    "revision": "c3c0487a6a896f3ccd09",
+    "url": "./static/js/runtime-main.5d2f63bd.js"
+  }
+]);
